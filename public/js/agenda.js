fac78d938deb02d118188b354814a02743d24d57
hernanofx <hernanofx@hotmail.com>
2016-09-13 03:25:02 -0300

    todo list with agenda

---
+++ public/js/agenda.js
@@ -0,0 +1,78 @@
+const EVENTS = [{
+    id: 1,
+    title: 'evento de ejemplo 1',
+    description: '',
+    dueDate: '2016-10-30T22:00:00.000Z'
+}, {
+    id: 2,
+    title: 'evento de ejemplo 2',
+    description: '',
+    dueDate: '2016-10-30T23:00:00.000Z'
+}, {
+    id: 3,
+    title: 'evento de ejemplo 3',
+    description: '',
+    dueDate: '2016-10-31T00:00:00.000Z'
+}, {
+    id: 4,
+    title: 'evento de ejemplo 4',
+    description: '',
+    dueDate: '2016-10-31T01:00:00.000Z'
+}, {
+    id: 5,
+    title: 'evento de ejemplo 5',
+    description: '',
+    dueDate: '2016-10-31T02:00:00.000Z'
+}, {
+    id: 6,
+    title: 'evento de ejemplo 6',
+    description: '',
+    dueDate: '2016-10-31T03:00:00.000Z'
+}, {
+    id: 7,
+    title: 'evento de ejemplo 7',
+    description: '',
+    dueDate: '2016-10-31T04:00:00.000Z'
+}, ];
+
+function addEventToAgenda(title, dueDate) {
+    const dayOfMonth = dueDate.getUTCDate();
+    const monthNumber = dueDate.getUTCMonth();
+    const year = dueDate.getUTCFullYear();
+    const dayOfWeek = dueDate.getUTCDay();
+
+    const hours = dueDate.getUTCHours();
+    const minutes = dueDate.getUTCMinutes();
+    const seconds = dueDate.getUTCSeconds();
+
+    jQuery('tbody').append(`
+    <tr>
+      <td class="agenda-date" class="active" rowspan="1">
+          <div class="dayofmonth">${dayOfMonth}</div>
+          <div class="dayofweek">${dayOfWeek}</div>
+          <div class="shortdate text-muted">${monthNumber + 1}, ${year}</div>
+      </td>
+      <td class="agenda-time">
+          ${hours}:${minutes}:${seconds}
+      </td>
+      <td class="agenda-events">
+          <div class="agenda-event">
+              <i class="glyphicon glyphicon-repeat text-muted" title="Repeating event"></i>
+              ${title}
+          </div>
+      </td>
+    </tr>
+  `);
+}
+
+jQuery('#add').on('click', function() {
+    const value = jQuery('#textInput').val();
+    addEventToAgenda(value, new Date ( ));
+});
+
+EVENTS.forEach(function(event) {
+
+    const d = new Date(event.dueDate);
+
+    addEventToAgenda(event.title, d);
+});
---
+++ public/js/calendario.js
@@ -0,0 +1,4 @@
+var dias: ['Lunes', 'Martes','Miercoles','Jueves','Viernes','Sabado','Domingo'],
+var diasab: ['Lun', 'Mar','Mie','Jue','Vie','Sab','Dom'],
+var meses: ['Enero', 'Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
+var mesesab: ['Ene', 'Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'],
